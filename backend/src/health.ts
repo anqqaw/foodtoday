f2ce9a0f70e059c7692985b028bc1333cad86d54
Aarni Klemetti <aarni.klemtti@gmail.com>
2025-01-29 14:18:40 +0200

    Deleted redis

--- backend/src/health.ts
+++ backend/src/health.ts
@@ -1,19 +1,8 @@
-import Redis from 'ioredis';
-
 import { prisma } from './prisma';
-import { config } from './config';
-
-const redis = new Redis(config.REDIS_URL, { family: 6 });
 
 export const check = async (ctx: any) => {
   try {
     await prisma.$queryRaw`SELECT 1`;
-
-    // Check Redis Connection
-    const redisStatus = await redis.ping();
-    if (redisStatus !== 'PONG') {
-      throw new Error('Redis is not responding');
-    }
 
     ctx.status = 200;
   } catch (error: any) {
