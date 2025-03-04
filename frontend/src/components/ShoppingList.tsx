351d9bb4646e14a48b8807ae1c0868c8f71fccbb
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-03-04 23:04:54 +0200

    feat: Added shoppinglist file

---
+++ frontend/src/components/ShoppingList.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { fetchShoppingList } from '../helpers/api';
+
+const ShoppingList: React.FC = () => {
+  const [shoppingList, setShoppingList] = useState<{ qty?: number; unit?: string; name: string }[]>([]);
+
+  const loadShoppingList = async () => {
+    try {
+      const list = await fetchShoppingList();
+      setShoppingList(list);
+    } catch (e) {
+      console.error('Error loading shopping list:', e);
+    }
+  };
+
+  useEffect(() => {
+    loadShoppingList();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-50 font-['Space_Grotesk'] flex justify-center items-center p-6">
+      {shoppingList.length > 0 ? (
+        <div className="text-lg text-gray-700 space-y-4">
+          {shoppingList.map((item, index) => (
+            <div key={index} className="leading-relaxed bg-white-100 p-3 rounded-lg w-full max-w-md shadow-lg rounded-xl p-6">
+              {item.qty && item.unit ? `${item.qty} ${item.unit} ${item.name}` : item.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-gray-500 text-center text-lg">Shopping list is empty</p>
+      )}
+    </div>
+  );
+};
+
+export default ShoppingList;
