deada6d59ade7dac7f841894c1291488cf7f3b49
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-03-05 14:17:24 +0200

    feat: Added handleRemove method

--- frontend/src/components/ShoppingList.tsx
+++ frontend/src/components/ShoppingList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchShoppingList } from '../helpers/api';
+import { fetchShoppingList, deleteFromShoppingList } from '../helpers/api';
 
 const ShoppingList: React.FC = () => {
   const [shoppingList, setShoppingList] = useState<{ qty?: number; unit?: string; name: string }[]>([]);
@@ -17,12 +17,25 @@ const ShoppingList: React.FC = () => {
     loadShoppingList();
   }, []);
 
+  const handleRemove = async (itemName: string) => {
+    try {
+      const updateList = await deleteFromShoppingList(itemName);
+      setShoppingList(updateList);
+    } catch (e) {
+      console.error('Error deleting item from shopping list:', e);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 font-['Space_Grotesk'] flex justify-center items-center p-6">
       {shoppingList.length > 0 ? (
         <div className="text-lg text-gray-700 space-y-4">
           {shoppingList.map((item, index) => (
-            <div key={index} className="leading-relaxed bg-white-100 p-3 rounded-lg w-full max-w-md shadow-lg rounded-xl p-6">
+            <div
+              key={index}
+              className="leading-relaxed bg-white-100 p-3 rounded-lg w-full max-w-md shadow-lg rounded-xl p-6"
+              onClick={() => handleRemove(item.name)}
+            >
               {item.qty && item.unit ? `${item.qty} ${item.unit} ${item.name}` : item.name}
             </div>
           ))}
