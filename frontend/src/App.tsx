55d948f1b519e3530df69529401b14da006e2fd4
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-03-19 17:00:35 +0200

    chore: Updated app.tsx to use Layout.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import DinnersView from './components/DinnersView';
 import LoginView from './components/LoginView';
 import DinnerDetails from './components/DinnerDetails';
 import DinnerList from './components/DinnerList';
 import ShoppingList from './components/ShoppingList';
-
-import BottomNavBar from './components/BottomNavBar';
-
-const Layout: React.FC = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-1 overflow-auto pb-20">
-        <Outlet />
-      </div>
-      <BottomNavBar />
-    </div >
-  );
-}
+import Layout from "./Layout";
 
 const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = React.useState<boolean>(false);
