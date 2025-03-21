854382c2d9de9b4c297d82f5d4c898ad1422989d
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-03-21 12:06:55 +0200

    chore: Switched route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -41,7 +41,7 @@ export function createApp() {
   privateRouter.get('/dinners/:id/addtoshoppinglist', dinners.addToShoppingList);
 
   privateRouter.get('/users/clearshoppinglist', users.clearShoppingList);
-  privateRouter.delete('/users/deletefromshoppinglist', users.deleteFromShoppingList);
+  privateRouter.delete('/users/shoppinglist', users.deleteFromShoppingList);
   privateRouter.get('/users/shoppinglist', users.getShoppingList);
 
   app.use(privateRouter.routes());
