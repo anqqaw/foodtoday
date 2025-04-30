231fb89ef8771b9a585bffdacb614390d42c2ac0
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-04-30 19:44:40 +0300

    chore: Removed setupEnv.ts

--- backend/tests/setupEnv.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-import { join } from 'path';
-
-if (process.env.NODE_ENV === 'test') {
-  const envFile = join(process.cwd(), '.env.test');
-  dotenv.config({ path: envFile });
-}
