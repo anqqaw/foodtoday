eafb9e1e8f2657d9f116f775196cc3a3c42fa0cc
Aarni Klemetti <aarni.klemetti@gmail.com>
2025-05-17 20:43:36 +0300

    chore: Removed unnecessary comment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Include all relevant files
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
